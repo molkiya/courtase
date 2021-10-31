3a1ae376950a6f54ccd1c90af726c9d60db44bc0
Marat Kiiamov <kiya.marat@yandex.ru>
2021-10-31 03:09:00 +0300

    Commit 2: Finished the layout and generating

---
+++ index.js
@@ -0,0 +1,53 @@
+const API_KEY = '207bd7f40150417e1002a825942ac75437ca6319';
+
+async function getResponse(results) {
+
+    const arrRandom = []
+
+    arrRandom.push(Math.floor(Math.random(0.7e6) * 1e6));
+
+    try {
+        results.length = 0;
+        try {
+            for (let randomCase of arrRandom) {
+                let response = await fetch(
+                    `https://api.case.law/v1/cases/${randomCase}/`,
+                    headers={'Authorization': API_KEY }
+                );
+                results.push(await response.json());
+            }
+        } catch {
+            alert(e)
+        }
+    } catch (e) {
+        console.log(new Error(e));
+    }
+}
+
+async function createElements() {
+    let results = [];
+    await getResponse(results);
+    results.forEach(item => {
+
+        const casesColumnContainer = document.querySelector('#casesColumnContainer');
+        const casesColumn = document.querySelector('#casesColumn');
+        const cases = document.querySelector('#cases');
+        const otherinfoContainer = document.querySelector('#otherinfoContainer');
+
+        cases.innerHTML = `
+        <h1>${item.name}</h1>
+        <h2>${item.court.name}</h2>
+        <a href="${item.frontend_url}">Link for case</a>
+        `
+
+        otherinfoContainer.innerHTML = `
+        <h2>Date: ${item.decision_date}</h2>
+        <h2>Number of case: ${item.docket_number}</h2>
+        `
+
+        casesColumnContainer.appendChild(casesColumn)
+        casesColumn.appendChild(cases)
+        cases.appendChild(otherinfoContainer)
+
+    })
+}
